f7b77eaeb7de1365e85a4ac2a507473ec5727c72
niuchaoqun <smallmenu@163.com>
2020-10-21 17:05:32 +0800

    * init

---
+++ src/test/java/cn/xexample/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package cn.xexample.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
